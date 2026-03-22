e64176170d35912190085963babed2f20363a8ae
FTL-Dave-Walsh <fixthislater@protonmail.com>
2026-03-22 12:16:41 -0400

    Shifted back to prefix text importing secrets by default

--- flake.nix
+++ flake.nix
@@ -20,17 +20,18 @@
           terraformWrapper = {
             package = pkgs.opentofu;
             extraRuntimeInputs = [ pkgs.jq ];
-#             # Not needed if envvars are already defined with all these credentials
-#             prefixText = ''
-#               TF_VAR_hcloud_api_token="$(cat /run/secrets/hcloud-api-token)"
-#               AWS_ACCESS_KEY_ID="$(cat /run/secrets/hcloud-s3-access-key)"
-#               AWS_SECRET_ACCESS_KEY="$(cat /run/secrets/hcloud-s3-secret-key)"
-#
-#               export TF_VAR_hcloud_api_token
-#               export AWS_ACCESS_KEY_ID
-#               export AWS_SECRET_ACCESS_KEY
-#               export AWS_DEFAULT_REGION="us-east-2"
-#             '';
+            # Assumes these 3 sops-nix secrets are already available and just need
+            # to be imported (safely) as envvars in the terranix shell
+            prefixText = ''
+              TF_VAR_hcloud_api_token="$(cat /run/secrets/hcloud-api-token)"
+              AWS_ACCESS_KEY_ID="$(cat /run/secrets/hcloud-s3-access-key)"
+              AWS_SECRET_ACCESS_KEY="$(cat /run/secrets/hcloud-s3-secret-key)"
+
+              export TF_VAR_hcloud_api_token
+              export AWS_ACCESS_KEY_ID
+              export AWS_SECRET_ACCESS_KEY
+              export AWS_DEFAULT_REGION="us-east-2"
+            '';
           };
           modules = [ ./config.nix ];
       };
